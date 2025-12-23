19f6007cbae86b66e98d067938a0abc8628c55a2
Sayem2004 <mdalimransayem@gmail.com>
2025-12-23 16:06:56 +0600

    Separate CSS file

---
+++ customer/view/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login-Bakery Hub</title>
+    <link rel="stylesheet" href="./../CSS/login.css">
+
+</head>
+<body>
+
+    <div class="form-container">
+        <h2>Login</h2>
+
+        <form>
+            <input type="email" placeholder="Email"><br>
+            <input type="password" placeholder="Password"><br>
+            <button type="submit">Login</button>
+        </form>
+
+        <p>Don't have an account? <a href="">Register here</a></p>
+    </div>
+
+</body>
+</html>
