a6a1358ddff0f7ffd9944f0f8c412e6d7d5ff31e
Sayem2004 <mdalimransayem@gmail.com>
2025-12-27 23:46:48 +0600

    Register Page Added

--- customer/view/login.php
+++ customer/view/login.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login-Bakery Hub</title>
     <link rel="stylesheet" href="./../CSS/login.css">
+    
 
 </head>
 <body>
@@ -21,7 +22,7 @@
             <button type="submit">Login</button>
         </form>
 
-        <p>Don't have an account? <a href="">Register here</a></p>
+        <p>Don't have an account? <a href="./register.php">Register here</a></p>
     </div>
 
 </body>
---
+++ customer/view/register.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Registration Form</title>
+    <link rel="stylesheet" href="./../CSS/register.css">
+
+   
+</head>
+
+<body>
+    <div  class="form-container">
+
+    <h2>Customer Registration Form</h2>
+
+    <div>
+        <form action="">
+            <label for="">Name</label><br>
+            <input type="text" placeholder ="Name"><br>
+
+            <label for="">Phone</label><br>
+            <input type="number" placeholder ="Phone"><br>
+            
+
+            <label for="">Email</label><br>
+            <input type="email" placeholder ="Email"><br>
+
+            <label for="">Password</label><br>
+            <input type="password" placeholder ="Password"><br>
+
+            <p>Already have an account? <a href="./login.php">Sign In</a></p>
+
+            <div class="flexx">
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+    </div>
+    </div>
+
+</body>
+
+</html>
